f14cf83b717fb8d5b9028ae4bd70126f5a2e5fc9
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2022-03-19 20:35:26 +0100

    O(n) implementation

--- containsDuplicate.js
+++ containsDuplicate.js
@@ -1,17 +1,24 @@
 function containsDuplicate1(nums) {
   for (let i = 0; i < nums.length; i++) {
     for (let j = i + 1; j < nums.length; j++) {
-        if(nums[i] === nums[j]){
-            return true;
-        }
-        return false;
+      if (nums[i] === nums[j]) {
+        return true;
+      }
+      return false;
     }
   }
   return undefined;
 }
 console.log(containsDuplicate1([1, 2, 3, 1]));
 
-// function containsDuplicate2(nums){
-//     map = {}
-
-// }
+//O(n) - much faster and has a lot of space
+function containsDuplicate2(nums) {
+  map = {};
+  for (let i = 0; i < nums.length; i++) {
+      if(map[nums[i]] !== undefined){
+          return true;
+      }
+      map[nums[i]] = i
+  }
+}
+console.log(containsDuplicate2([1, 2, 3, 1]))
