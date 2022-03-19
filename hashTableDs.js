4d549e2d69a3e38213385152162eac65e0ac0043
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2022-03-19 19:16:49 +0100

    Implementation of the hash table data structure

---
+++ hashTableDs.js
@@ -0,0 +1,52 @@
+class HashTable {
+  constructor(size) {
+    this.data = new Array(size);
+  }
+
+  _hash(key) {
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+      hash = (hash + key.charCodeAt(i) * i) & this.data.length;
+    }
+    return hash;
+  }
+  set(key, value) {
+    let address = this._hash(key);
+    if (!this.data[address]) {
+      this.data[address] = [];
+      this.data[address].push([key, value]);
+    }
+    this.data[address].push([key, value]);
+    return this.data;
+  }
+
+  get(key) {
+    let address = this._hash(key);
+    const currentBucket = this.data[address];
+    console.log(currentBucket);
+    if (currentBucket) {
+      for (let i = 0; i < currentBucket.length; i++) {
+        if (currentBucket[i][0] === key) {
+          return currentBucket[i][0][0];
+        }
+      }
+    }
+    return undefined;
+  }
+
+  keys() {
+    const keysArray = [];
+    for (let i = 0; i < this.data.length; i++) {
+        if(this.data[i]){
+            keysArray.push(this.data[i][0][0])
+        }
+    }
+    return keysArray;
+  }
+}
+
+const myHashTable = new HashTable(50);
+myHashTable.set("grapes", 1000);
+myHashTable.set("apples", 54);
+myHashTable.set("oranges", 2);
+console.log(myHashTable.keys());
