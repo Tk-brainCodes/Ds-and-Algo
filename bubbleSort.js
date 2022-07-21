60d2aa3d7ac773747a08c74f7c2f56a58334c27e
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2022-07-21 09:08:50 +0100

    Feature: Bubble Sort Algorithm 0(n2)

---
+++ bubbleSort.js
@@ -0,0 +1,18 @@
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+function bubblSort1(arr) {
+  const length = arr.length;
+  for (let i = 0; i < length; i++) {
+    for (let j = 0; j < length; j++) {
+      if (arr[j] > arr[j + 1]) {
+        //swap numbers
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+}
+
+bubblSort1(numbers);
+console.log(numbers);
