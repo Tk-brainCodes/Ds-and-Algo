a1732409f67a056ce28f52564c263423a14d9a8a
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2022-07-25 12:07:21 +0100

    Feature: Merge sort algorithm O(n)

---
+++ mergeSort.js
@@ -0,0 +1,46 @@
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+// function mergeSort(array) {
+//   //base case
+//   if (array.length <= 1) {
+//     return array;
+//   }
+
+//   return mergeSort(merge(left), merge(right));
+
+//   function merge(left, right) {}
+// }
+
+// const answer = mergeSort(numbers);
+// console.log(answer);
+
+//merge helper function
+function merge(left, right) {
+  let sortedArray = [];
+
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      sortedArray.push(left.shift());
+    } else {
+      sortedArray.push(right.shift());
+    }
+  }
+  return [...sortedArray, ...left, ...right];
+}
+
+function mergeSort2(array) {
+  //base case
+  if (array.length <= 1) return array;
+  //half the array
+  const mid = Math.floor(array.length / 2);
+  //get first half recursively
+  let left = mergeSort2(array.slice(0, mid));
+  //get second half recursively
+  let right = mergeSort2(array.slice(mid));
+
+  console.log("pos:", left, right);
+
+  return merge(left, right);
+}
+
+console.log(mergeSort2(numbers));
